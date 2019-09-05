8f6f3fcd0311d9cfb1330f4fcf1830dc29034e05
Ubuntu <ubuntu@ip-172-31-86-57.ec2.internal>
2019-09-05 19:55:36 +0000

    still attempting to get genius

---
+++ templates/genius.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import os
+import requests
+import json
+import random
+import flask
+import os
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')  
+def index(): 
+    return flask.render_template("index.html")
+    
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
+
+
+
+url =  "https://api.genius.com/search"
+
+my_headers = {
+    "Authorization": "Bearer cG7siougj_ooBlmUJ7t6TMK0LLArlMM2DjXpUwzQ3uQf4gQ-vYTMlIVrHHhq9-Tc"
+}
+
+
+
+artist = "Frank Ocean"
+song_title = "White Ferrari"
+data = {'q': song_title + ' ' + artist}
+
+
+response = requests.get(url, data=data, headers=my_headers)
+json_body = response.json()
+
+
+
+print(json.dumps(json_body, indent=2))
